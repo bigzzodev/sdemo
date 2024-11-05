e20911c1edc4f0fda2936fdca4d09b3a047a770b
bigzzodev <bigzzodev@gmail.com>
2024-11-05 15:48:00 +0900

    i

--- app.py
+++ app.py
@@ -2,13 +2,30 @@ import streamlit as st
 from analytics import ST_ANALYTICS
 
 # ------------------------------------------------------------------------------------------------------------------------
-def main():
+def main1():
     with st.sidebar:
         st.subheader("뉴스기사 분석 시스템")
         singer_opt = list(ST_ANALYTICS.keys())
-        selected_company = st.selectbox(label="회사를 선택하세요", options=singer_opt,)
+        selected_company = st.selectbox(label="회사를 선택하세요", options=singer_opt, key="main_button1")
         func, year_opt = ST_ANALYTICS[selected_company]
     func(selected_company)
+
+# ------------------------------------------------------------------------------------------------------------------------
+def main2():
+    with st.sidebar:
+        st.divider()
+        st.subheader("자동 분석 리포팅 생성")
+        st.caption("아래 리포팅은 사람의 개입없이 매일 동일한시간에 자동으로 :blue[주가챠트를 캡쳐]하여 :blue[기술적 분석 리포팅 생성] 하는 예제 입니다")
+        pdf_file_path = "./sdemo_sample.pdf"
+        # PDF 파일을 바이너리로 읽기
+        with open(pdf_file_path, "rb") as f:
+            pdf_content = f.read()
+        st.download_button(
+            label="Download PDF",
+            data=pdf_content,
+            file_name="sample.pdf",
+            mime="application/pdf"
+        )
 
 # ------------------------------------------------------------------------------------------------------------------------
 if __name__ == '__main__':
@@ -26,4 +43,6 @@ if __name__ == '__main__':
     with open('style.css') as f:
         st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
 
-    main()
+    main1()
+    
+    main2()
