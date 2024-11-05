383321e82285fe1f031ed4b891ca7dfdec13b5d2
bigzzodev <bigzzodev@gmail.com>
2024-11-05 10:07:34 +0900

    i

--- analytics/company.py
+++ analytics/company.py
@@ -2,7 +2,7 @@ from streamlit_echarts import JsCode
 from streamlit_echarts import st_echarts
 
 from .dashboard_news import all_dashboard_news
-from .dashboard_company import all_dashboard_company
+from .dashboard_company import all_dashboard_company, ready_dashboard_company
 
 ST_COMPANY = {
     # 페이지 옵션으로 뜨는 부분
@@ -10,7 +10,17 @@ ST_COMPANY = {
 
     "삼성생명": (
         all_dashboard_company,
-        ["2020", "2021"],
+        ["2020", "2021", "2022", "2023", "2024"],
+    ),
+
+    "교보생명": (
+        ready_dashboard_company,
+        ["2020", "2021", "2022", "2023", "2024"],
+    ),
+
+    "한화생명": (
+        ready_dashboard_company,
+        ["2020", "2021", "2022", "2023", "2024"],
     ),
 
 }
--- analytics/dashboard_company.py
+++ analytics/dashboard_company.py
@@ -161,55 +161,6 @@ def _is_skip_class(_json_data, _target, _class):
             if _class in urls:
                 return True, urls[_class]  # True와 클래스 값 반환
     return False, None
-# ------------------------------------------------------------------------------------------------------------------------
-def all_dashboard_company2(_singer):
-
-    # from streamlit_autorefresh import st_autorefresh
-    #Time
-    nowTime = datetime.now()
-    current_time = nowTime.strftime("%H:%M:%S")
-    today = str(date.today())
-    # st.write(today)
-    timeMetric,= st.columns(1)
-    timeMetric.metric("",today)
-
-    # Row A
-    a1, a2, a3 = st.columns(3)
-    # a1.image(logo)
-    a2.metric("Stockholm Temperature", f"{11}", f"{22}"+"%")
-    a3.metric("Stockholm time", current_time)
-
-
-    # Row B
-    b1, b2, b3, b4 = st.columns(4)
-    b1.metric("Humidity", f"{get_humidity()}"+"%")
-    b2.metric("Feels like", f"{get_feel()}")
-    b3.metric("Highest temperature", f"{get_temp_max()}")
-    b4.metric("Lowest temperature", f"{get_temp_min()}")
-
-    # Row C
-    #C1 being the graph, C2 The Table.
-    c1, c2 = st.columns((7,3))
-
-    #Graph:
-    with c1:
-
-        chart_data = pd.DataFrame(
-            np.random.randn(20, 3),
-            columns=['Low', 'High', 'Close'])
-        st.line_chart(chart_data)
-
-    #The fake nonsens table:
-    with c2:
-        df = pd.DataFrame(
-            np.random.randn(7, 5),
-            columns=('Paris','Malta','Stockholm','Peru','Italy')
-        )
-
-        st.table(df)
-
-    #Manually refresh button
-    st.button("Run me manually")
 
 # ------------------------------------------------------------------------------------------------------------------------
 def all_dashboard_company(_company):
@@ -485,5 +436,14 @@ def class_dashboard(day_data, _dname, _class):
                 st.write(reason_list[idx])
 
     st.divider()
+
+
+# ------------------------------------------------------------------------------------------------------------------------
+def ready_dashboard_company(_company):
+    st.subheader(f'분석할 회사 : {_company}')
+
+    st.write('예시 페이지 입니다.')
+
+
 
 # eof
